32b4f4d81a7b7a33d4a01bc32ffc512f0bf8c110
davidmansion <contact@davidmansion.com>
2024-04-20 19:17:14 +0100

    fifth commit

---
+++ components/providers/web3/index.tsx
@@ -0,0 +1,43 @@
+import { FunctionComponent, ProviderProps, ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Contract, providers } from "ethers";
+import { Web3State, createDefaultState } from "./utils";
+import { ethers } from "ethers";
+
+interface Web3ProviderProps {
+    children: ReactNode;
+  }
+
+const Web3Context = createContext<Web3State>(createDefaultState());
+
+const Web3Provider: FunctionComponent<Web3ProviderProps> = ({children}) => {
+    const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState())
+
+    useEffect(() => {
+        function initWeb3() {
+
+            const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+            
+            setWeb3Api({
+                ethereum: window.ethereum,
+                provider,
+                contract: null,
+                isLoading: false
+            })
+        }
+
+        initWeb3();
+    }, [])
+
+    return (
+        <Web3Context.Provider value={web3Api}>
+            {children}
+        </Web3Context.Provider>
+    )
+}
+
+export function useWeb3() {
+    return useContext(Web3Context);
+}
+
+export default Web3Provider;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 
 
-import "../styles/globals.css"
-import type { AppProps } from "next/app"
+import { Web3Provider } from "components/providers";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
     </>
   )
   
