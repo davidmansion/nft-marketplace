c9907c9c5781b23e015d68851507032af786453b
papaexchange <info@papaexchange.com>
2024-04-25 19:17:05 +0100

    eighth commit v2

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -64,12 +64,14 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                
                 <Walletbar
                 isInstalled={account.isInstalled} 
                 isLoading={account.isLoading}
                 connect={account.connect}
                 account={account.data}
                 />
+                
               </div>
             </div>
           </div>
